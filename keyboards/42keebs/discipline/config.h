0e6daea83dda1cff269f9ec22d013d0c010fedef
Leonard Freimuth <leonard.freimuth@gmail.com>
2022-04-10 23:06:16 +0200

    new layou+keymap

--- keyboards/42keebs/discipline/config.h
+++ keyboards/42keebs/discipline/config.h
@@ -101,7 +101,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
  * This is userful for the Windows task manager shortcut (ctrl+shift+esc).
  */
-// #define GRAVE_ESC_CTRL_OVERRIDE
+#define GRAVE_ESC_CTRL_OVERRIDE
 
 /*
  * Force NKRO
