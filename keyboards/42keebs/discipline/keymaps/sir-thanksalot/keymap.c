facd3cb4aa086c19fd70eb27339c20f59fdc0634
Leonard Freimuth <leonard.freimuth@gmail.com>
2022-04-12 21:38:41 +0200

    update keymap

--- keyboards/42keebs/discipline/keymaps/sir-thanksalot/keymap.c
+++ keyboards/42keebs/discipline/keymaps/sir-thanksalot/keymap.c
@@ -27,25 +27,25 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_65_ansi_iso_lshift(
-    //  esc      1        2        3        4        5        6        7        8        9        0        -        =        bkspc    delete 
+    //  esc      1        2        3        4        5        6        7        8        9        0        -        =        bkspc    delete
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
-    //  tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        page up 
+    //  tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        page up
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
     //  caps     A        S        D        F        G        H        J        K        L        ;        '             enter        pg down
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,      KC_ENT,      KC_PGDN,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,      KC_ENT,      KC_PGDN,
     //  shift    soup     Z        X        C        V        B        N        M        ,        .        /        shift    up       end
-        KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
+        KC_LSFT, MO(_FN), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
     //  ctrl     win      alt                              space                         alt      fn       ctrl     left     down     right
         KC_LCTL, KC_LGUI, KC_LALT,                         KC_SPC,                       KC_RALT, MO(_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
     [_FN] = LAYOUT_65_ansi_iso_lshift(
-    //  esc      1        2        3        4        5        6        7        8        9        0        -        =        bkspc    delete 
+    //  esc      1        2        3        4        5        6        7        8        9        0        -        =        bkspc    delete
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_INS,
-    //  tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        page up 
+    //  tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        page up
         _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     //  caps     A        S        D        F        G        H        J        K        L        ;        '             enter        pg down
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,     _______,
+        KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,      _______,     _______,
     //  shift    soup     Z        X        C        V        B        N        M        ,        .        /        shift    up       end
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLU, KC_HOME,
     //  ctrl     win      alt                              space                         alt      fn       ctrl     left     down     right
@@ -56,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /*
     [1] = LAYOUT_65_ansi_iso_lshift(
-    //  esc      1        2        3        4        5        6        7        8        9        0        -        =        bkspc    delete 
+    //  esc      1        2        3        4        5        6        7        8        9        0        -        =        bkspc    delete
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    //  tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        page up 
+    //  tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        page up
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     //  caps     A        S        D        F        G        H        J        K        L        ;        '             enter        pg down
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,     _______,
