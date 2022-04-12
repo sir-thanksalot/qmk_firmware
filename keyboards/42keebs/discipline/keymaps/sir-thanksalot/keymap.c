6112a9e8d11fdc4f398f77e89de27208323d5beb
Leonard Freimuth <leonard.freimuth@gmail.com>
2022-04-12 20:21:48 +0200

    update keymap

--- keyboards/42keebs/discipline/keymaps/sir-thanksalot/keymap.c
+++ keyboards/42keebs/discipline/keymaps/sir-thanksalot/keymap.c
@@ -17,8 +17,16 @@
 
 // use atmega32a in QMK Toolkit
 
+
+enum {
+    _BASE = 0,
+    _FN,
+};
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_65_ansi_iso_lshift(
+    [_BASE] = LAYOUT_65_ansi_iso_lshift(
     //  esc      1        2        3        4        5        6        7        8        9        0        -        =        bkspc    delete 
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
     //  tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        page up 
@@ -28,34 +36,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //  shift    soup     Z        X        C        V        B        N        M        ,        .        /        shift    up       end
         KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
     //  ctrl     win      alt                              space                         alt      fn       ctrl     left     down     right
-        KC_LCTL, KC_LGUI, KC_LALT,                         KC_SPC,                       KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                         KC_SPC,                       KC_RALT, MO(_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
-    [1] = LAYOUT_65_ansi_iso_lshift(
+    [_FN] = LAYOUT_65_ansi_iso_lshift(
     //  esc      1        2        3        4        5        6        7        8        9        0        -        =        bkspc    delete 
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_INS,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_INS,
     //  tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        page up 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     //  caps     A        S        D        F        G        H        J        K        L        ;        '             enter        pg down
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,     _______,
     //  shift    soup     Z        X        C        V        B        N        M        ,        .        /        shift    up       end
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLU, KC_HOME,
     //  ctrl     win      alt                              space                         alt      fn       ctrl     left     down     right
-        KC_TRNS, KC_TRNS, KC_TRNS,                         KC_TRNS,                      KC_RALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______, _______, _______,                         _______,                      _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
     )
+
+
 
     /*
     [1] = LAYOUT_65_ansi_iso_lshift(
     //  esc      1        2        3        4        5        6        7        8        9        0        -        =        bkspc    delete 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     //  tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        page up 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     //  caps     A        S        D        F        G        H        J        K        L        ;        '             enter        pg down
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,     _______,
     //  shift    soup     Z        X        C        V        B        N        M        ,        .        /        shift    up       end
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     //  ctrl     win      alt                              space                         alt      fn       ctrl     left     down     right
-        KC_TRNS, KC_TRNS, KC_TRNS,                         KC_TRNS,                      KC_RALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______, _______, _______,                         _______,                      _______, _______, _______, _______, _______, _______
     )
     */
 };
@@ -68,3 +78,5 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     return true;
 }
+
+
